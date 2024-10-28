abdce597e67c1404885313fbe6fae4e7697b7c86
areloyann <araqseloyan23@gmail.com>
2024-10-28 14:59:50 +0400

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <cmath> 
 using namespace std;
 int gcd(int a, int b) {
+    a = abs(a); 
+    b = abs(b); 
     while (b != 0) {
         int temp = b;
         b = a % b;
@@ -8,14 +11,12 @@ int gcd(int a, int b) {
     }
     return a;
 }
-
 int main() {
     int num1, num2;
     cout << "Nermuceq tvery: ";
     cin >> num1 >> num2;
-
     int result = gcd(num1, num2);
     cout << "Amenamec yndhanur bajanarary: " << result << endl;
-
     return 0;
 }
+
